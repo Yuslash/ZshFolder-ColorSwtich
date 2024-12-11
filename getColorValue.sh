4223cca36f3195d02723a487559e4c6316787ed1
Yuslash <sibikrishna71@gmail.com>
2024-12-11 13:17:03 +0530

    we did can able to access the swtichColor to getColorvalue

--- getColorValue.sh
+++ getColorValue.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #print the code argument you got from the first switchScript
-if [ "$COLOR_CODE"="invalid" ]; then
+if [ "$COLOR_CODE" = "invalid" ]; then
   echo -e "\e[31mInvalid Color Input\e[0m"
 else
-  echo "\e[${COLOR_CODE}m the color code is: $COLOR_CODE"
+  echo -e "\e[${COLOR_CODE}m the color code is: $COLOR_CODE\e[0m"
 fi
